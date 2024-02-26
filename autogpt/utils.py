a5606859a998b81c4bf65f285466a0b9ccfdcddf
hejl <hjlarry@163.com>
2024-02-26 13:12:23 +0800

    add the data to process

--- autogpt/utils.py
+++ autogpt/utils.py
@@ -5,6 +5,7 @@ THOUGHT_COLOR = Fore.GREEN
 OBSERVATION_COLOR = Fore.YELLOW
 ROUND_COLOR = Fore.BLUE
 
+
 def pprint(text, color=None, end="\n"):
     if color is not None:
         content = color + text + Style.RESET_ALL + end
